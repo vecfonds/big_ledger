d834e2221de14d20f387564ba31f30a4bfaeef8b
Long <long01639637721@gmail.com>
2024-03-10 10:00:42 +0700

    delete unused modules

--- src/mudules/location/dto/create-comment.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsString, IsNotEmpty, IsNumber } from 'class-validator';
-
-export class CreateCommentDto {
-  @IsNotEmpty({ message: 'content is required' })
-  @IsString({ message: 'content must be string' })
-  @ApiProperty({ example: 'beautiful' })
-  content: string;
-}
--- src/mudules/location/dto/search-by-name.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsString, IsNotEmpty, IsEmail } from 'class-validator';
-
-export class SearchByNameDto {
-  @IsNotEmpty({ message: 'name is required' })
-  @IsString({ message: 'name must be string' })
-  @ApiProperty({ example: 'TPHCM' })
-  name: string;
-}
