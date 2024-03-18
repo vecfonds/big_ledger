005cdf0aae975fd469c2370f5cde23e87ba3d7b2
Long <long01639637721@gmail.com>
2024-03-18 21:39:39 +0700

    Update payment method constants

--- src/constants/payment-method.ts
+++ src/constants/payment-method.ts
@@ -1,7 +1,7 @@
 import { type ValueOf } from '../interfaces';
 export const PAYMENT_METHOD = {
-  TIEN_MAT: 'TIEN_MAT',
-  CHUYEN_KHOAN: 'CHUYEN_KHOAN',
+  CASH: 'CASH',
+  TRANSFER: 'TRANSFER',
 } as const;
 
 export type PaymentMethodType = ValueOf<typeof PAYMENT_METHOD>;
