7c7d85941f4c4a08b01f6a4b8e05c767984e3378
Long <long01639637721@gmail.com>
2024-05-15 23:51:45 +0700

    feat(product): add category field to UpdateProductDto

--- src/mudules/product/dto/update-product.dto.ts
+++ src/mudules/product/dto/update-product.dto.ts
@@ -1,6 +1,13 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateProductDto, CreateProductGroupDto } from './create-product.dto';
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { IsNumber, IsOptional } from 'class-validator';
 
-export class UpdateProductDto extends PartialType(CreateProductDto) {}
+export class UpdateProductDto extends PartialType(CreateProductDto) {
+  @ApiPropertyOptional({ type: Number, default: 100 })
+  @IsNumber(undefined, { message: 'category must be a number' })
+  @IsOptional()
+  category: number;
+}
 
 export class UpdateProductGroupDto extends PartialType(CreateProductGroupDto) {}
